4e9cc0e67588a0a12932d6fadb4cfd11d4a95280
JAYAVARMANJ <115263270+JAYAVARMANJ@users.noreply.github.com>
2022-11-07 12:12:14 +0530

    Create ques 5.js

---
+++ ques 5.js
@@ -0,0 +1,8 @@
+# SDE-task-2
+
+console.log(str.split());
+console.log(str.split('e'));
+console.log(str.split(' '));
+console.log(str.split(''));
+
+console.log(cargoHold.split(',').sort().join(','));
